9fb994f95d0cbec6a34bc8b2307554d3742eba1d
KlKrzysztof <115008405+KlKrzysztof@users.noreply.github.com>
2025-05-05 20:00:53 +0200

    Added some modules Vehicles panel created Basic List created

--- FleetManager/fleetmanager.client/src/main.tsx
+++ FleetManager/fleetmanager.client/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Navbar from './Navbar'
+import Vehicles from './Vehicles'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Navbar/>
+    <StrictMode>
+        <Vehicles />
   </StrictMode>,
 )
