3027ed483e2a5d1e5551c2727bf3abbb583124ca
KlKrzysztof <115008405+KlKrzysztof@users.noreply.github.com>
2025-06-23 11:54:34 +0200

    Partialy completed vehicles

--- FleetManager/fleetmanager.client/src/Vehicles/VehiclesDetails.tsx
+++ FleetManager/fleetmanager.client/src/Vehicles/VehiclesDetails.tsx
@@ -16,15 +16,53 @@ function VehiclesDetails({ id }: getVehicle) {
     var [PlateNr, setPlateNr] = useState("")
     var [Purpose, setPurpose] = useState("")
     var [TotalMileage, setTotalMileage] = useState("")
-    var [Outfit, setOutfit] = useState("")
+    var [Outfit, setOutfit] = useState([])
     var [Overseerer, setOverseerer] = useState("")
     var [Serviced, setServiced] = useState(false)
     var [Vehicle, setVehicle] = useState("")
+    var [Vin, setVin] = useState("")
+    var [vehiclePurposeId, setVehiclePurposeId] = useState(0)
+    var [modelId, setModelId] = useState(0)
+
+    var [OutfittingOptions, setOutfittingOption] = useState([])
+    var [PurposeOptions, setPurposeOptions] = useState([])
+    var [OverseererOptions, setOverseererOptions] = useState([])
+
+    var [showOutfittingOptions, setshowOutfittingOptions] = useState(false)
+    var [showPurposeOptions, setShowPurposeOptions] = useState(false)
+    var [showOverseererOptions, setshowOverseererOptions] = useState(false)
+
+    var [purposeListHandler, setPurposeListHandler] = useState([false])
+    var [outfittingListHandler, setOutfittingListHandler] = useState([false])
+    var [overseererListHandler, setOverseererListHandler] = useState([false])
+
+    var [PurposeOptionsHover, setPurposeOptionsHover] = useState(false)
+    var [overseererOptionsHover, setOvereererOptionsHover] = useState(false)
 
     const handleModifyVehicle = async (id: number | null) => {
 
         if (canModify) {
-            fetch(`/api/vehicle/vehicle/update/`, {
+
+            const purposeExist = PurposeOptions.some(p => p.name == Purpose)
+
+            var puprId = vehiclePurposeId
+
+            if (!purposeExist) {
+                await fetch(`/api/vehicle/purpose/create/`, {
+                    method: "PUT",
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        vehiclePurposeId: 0,
+                        name: Purpose
+                    })
+                })
+                setVehiclePurposeId(PurposeOptions.length + 1)
+                puprId = PurposeOptions.length + 1
+            }
+
+            const z = await fetch(`/api/vehicle/vehicle/update/`, {
                 method: "POST",
                 headers: {
                     'Content-type': 'application/json'
@@ -33,10 +71,10 @@ function VehiclesDetails({ id }: getVehicle) {
                     vehicleId: Number(id),
                     totalMileage: Number(TotalMileage),
                     isInService: Boolean(Serviced),
-                    vehiclePurposeId: 1,//Purpose,
+                    vehiclePurposeId: puprId,
                     plateNumber: PlateNr,
-                    modelId: 1,
-                    vin: "420"
+                    modelId: modelId,
+                    vin: Vin
                 })
             })
 
@@ -91,143 +129,120 @@ function VehiclesDetails({ id }: getVehicle) {
             if (id == null || id == undefined)
                 return
 
-            const response = await fetch("/api/vehicle/vehicle/id/" + id)
+            const response = await fetch("/api/vehicle/vehicle/details/id/" + id)
             const json: JSON = await response.json()
 
             const {
                 vehicleId,
+                vehicle,
+                vehicleModel,
+                vehicleManufacturer,
+                vehicleOutfitting,
+                vehiclePurpose,
+                vehicleVersion
+            } = json;
+
+            const {
+                _,
                 totalMileage,
                 isInService,
                 vehiclePurposeId,
                 plateNumber,
                 modelId,
                 vin
-            } = json;
+            } = vehicle
 
             setPlateNr(plateNumber)
-            setOutfit("")
 
-            try {
-                await fetch("/api/vehicle/purpose/id/" + vehiclePurposeId).then(res => res.json()).then(data => setPurpose(data.name))
-            }
-            catch (ex) {
-                setPurpose("Not available")
-            }
-            try {
-                await fetch("/api/vehicle/model/id/" + modelId).then(res => res.json()).then(async data => {
-                    const Manufacturer = await fetch("/api/vehicle/manufacturer/id/" + data.manufacturerId).then(res => res.json())
-                    const Version = await fetch("/api/vehicle/version/id/" + data.vehicleVersionId).then(res => res.json())
+            setOutfit(vehicleOutfitting)
 
-                    const Outfits = await fetch("/api/vehicle/outfitting/all" + data.vehicleVersionId).then(res => res.json())
-                    /*const OutfitsArray
-                    Outfits*/
+            setVin(vehicle.vin)
 
-                    setVehicle(String(Manufacturer.name + Version.name))
-                    setOutfit(Outfit)
-                })
-            }
-            catch (ex) {
-                setVehicle("Not available")
-            }
+            setVehicle(`${vehicleManufacturer.name} ${vehicleVersion.name}`)
             
             setTotalMileage(totalMileage)
             
             setServiced(isInService)
+
+            setPurpose(vehiclePurpose.name)
+
+            setVehiclePurposeId(vehiclePurposeId)
+
+            setModelId(modelId)
+
+            try {
+                const overseererCareTake = await fetch("/api/caretake/get/vehicle/id/" + vehicleId).then(res => res.json())
+
+                const overseerer = await fetch("api/employee/get/id/" + overseererCareTake.employeeId).then(res => res.json())
+
+                setOverseerer(`${overseerer.firstName} ${overseerer.surname}`)
+            }
+            catch (ex) {
+                setOverseerer("Not available")
+            }
         }
         handleGetData(id)
+
+        const handleGetOptions = async () => {
+            const resultOut = await fetch("/api/vehicle/outfitting/all").then(res => res.json())
+
+            setOutfittingOption(resultOut)
+
+            setOutfittingListHandler(Array(resultOut.length).fill(false))
+
+            const resultPurp = await fetch("/api/vehicle/purpose/all").then(res => res.json())
+
+            setPurposeOptions(resultPurp)
+
+            setPurposeListHandler(Array(resultPurp.length).fill(false))
+
+            const resultEmp = await fetch("/api/employee/get/all").then(res => res.json())
+
+            setOverseererOptions(resultEmp)
+
+            setOverseererListHandler(Array(resultEmp.length).fill(false))
+        }
+        handleGetOptions()
+
     }, [id])
 
-    const handleDeleteVehicle = async (id: number) => {
-        await fetch(`/api/vehicle/vehicle/delete/id/${id}`, {
-            method: "DELETE"
-        })
+    useEffect(() => {
+        
+    }, [])
+
+    const setPurposeHover = (id: number) => {
+        var arr = Array(purposeListHandler.length).fill(false)
+
+        arr[id] = true
+
+        setPurposeListHandler(arr)
     }
 
-    useEffect(() => {
-        const handleGetData = async (id: Number | null | undefined) => {
-            if (id == null || id == undefined)
-                return
-
-            const response = await fetch("/api/vehicle/vehicle/id/" + id)
-            const json: JSON = await response.json()
-
-            const {
-                vehicleId,
-                totalMileage,
-                isInService,
-                vehiclePurposeId,
-                plateNumber,
-                modelId,
-                vin
-            } = json;
-
-            setPlateNr(plateNumber)
-            setOutfit("")
-
-            try {
-                await fetch("/api/vehicle/purpose/id/" + vehiclePurposeId).then(res => res.json()).then(data => setPurpose(data.name))
+    const setOnCLickPurpose = () => {
+        for (let i = 0; i < purposeListHandler.length; i++) {
+            if (purposeListHandler[i] == true) {
+                setVehiclePurposeId(PurposeOptions[i].vehiclePurposeId)
+                setPurpose(PurposeOptions[i].name)
             }
-            catch (ex) {
-                setPurpose("Not available")
-            }
-            try {
-                await fetch("/api/vehicle/model/id/" + modelId).then(res => res.json()).then(async data => {
-                    const Manufacturer = await fetch("/api/vehicle/manufacturer/id/" + data.manufacturerId).then(res => res.json())
-                    const Version = await fetch("/api/vehicle/version/id/" + data.vehicleVersionId).then(res => res.json())
-
-                    const Outfits = await fetch("/api/vehicle/outfitting/all" + data.vehicleVersionId).then(res => res.json())
-                    /*const OutfitsArray
-                    Outfits*/
-
-                    setVehicle(String(Manufacturer.name + Version.name))
-                    setOutfit(Outfit)
-                })
-            }
-            catch (ex) {
-                setVehicle("Not available")
-            }
-            
-            setTotalMileage(totalMileage)
-            
-            setServiced(isInService)
         }
-        handleGetData(id)
-    }, [id])
+    }
 
-    useEffect(() => {
-        const handleGetData = async (id: Number | null | undefined) => {
-            if (id == null || id == undefined)
-                return
+    const setOverseererHover = (id: number) => {
+        var arr = Array(overseererListHandler.length).fill(false)
 
-            const response = await fetch("/api/vehicle/vehicle/id/" + id)
-            const json: JSON = await response.json()
+        arr[id] = true
 
-            const {
-                vehicleId,
-                totalMileage,
-                isInService,
-                vehiclePurposeId,
-                plateNumber,
-                modelId,
-                vin,
-                caretakes,
-                model,
-                vehiclePurpose
-            } = json;
+        setOverseererListHandler(arr)
+    }
 
-            setPlateNr(plateNumber)
-            setOutfit("")
-
-            if (caretakes.id != null)
-                await fetch("/api/employees/get/id/" + caretakes.id).then(res => res.json()).then(data => setOverseerer(data.firstName + data.surname))
-
-            setPurpose(vehiclePurpose)
-            setTotalMilage(totalMileage)
-            setVehicle(model)
-            setServiced(isInService)
+    const setOnCLickOvereerer = () => {
+        for (let i = 0; i < overseererListHandler.length; i++) {
+            if (overseererListHandler[i] == true) {
+                setVehiclePurposeId(OverseererOptions[i].vehiclePurposeId)
+                setPurpose(PurposeOptions[i].name)
+            }
         }
-        handleGetData(id)
-    }, [id])
+    }
 
     const panelStyle: React.CSSProperties = {
         height: '45vh',
@@ -346,6 +361,23 @@ function VehiclesDetails({ id }: getVehicle) {
         fontSize: '20px'
     }
 
+    const listStyle = {
+        position: "relative",
+        top: "0",
+        left: "0",
+        right: "0",
+        zIndex: '1000',
+        background: 'white',
+        border: '1px solid #ccc',
+        borderTop: 'none',
+        maxHeight: '200px',
+        overflowY: 'auto',
+        listStyle: 'none',
+        padding: '0',
+        margin: '0',
+        boxShadow: '0 4px 8px rgba(0,0,0,0.1)'
+    } as React.CSSProperties
+
     return <div style={panelStyle} >
         <input style={nameInputStyle} type="text" value={Vehicle} onChange={canModify ? (e) => setVehicle(e.target.value) : undefined}></input>
         <div style={wraperStyle }>
@@ -356,7 +388,18 @@ function VehiclesDetails({ id }: getVehicle) {
                 </div>
                 <div style={inputWraperStyle}>
                     Puropse
-                    <input type="text" style={inputStyle} value={Purpose} onChange={canModify ? (e) => setPurpose(e.target.value) : undefined}></input>
+                    <input type="text" style={inputStyle} value={Purpose} onChange={canModify ? (e) => setPurpose(e.target.value) : undefined} onClick={canModify ? () => setShowPurposeOptions(true) : undefined} onBlur={canModify ? () => setShowPurposeOptions(false) : undefined}></input>
+                    {
+                        showPurposeOptions || PurposeOptionsHover ? <ul style={listStyle} onMouseEnter={() => { setPurposeOptionsHover(true) }} onMouseLeave={() => { setPurposeOptionsHover(false) } }>
+                            {PurposeOptions.map(p => (
+                                <li key={p.vehiclePurposeId} style={!purposeListHandler[p.vehiclePurposeId] ? { cursor: "pointer" } : { cursor: "pointer", backgroundColor: "#cacaca" }} onMouseEnter={() => setPurposeHover(p.vehiclePurposeId)} onClick={() => setOnCLickPurpose() }>{p.name}</li>
+                            ))}
+                            {/*<li style={{ cursor: "pointer" }} >1</li>
+                            <li>2</li>
+                            <li>3</li>*/}
+                        </ul>
+                        : undefined
+                    }
                 </div>
                 <div style={inputWraperStyle}>
                     Total mileage
@@ -365,12 +408,23 @@ function VehiclesDetails({ id }: getVehicle) {
             </div>
             <div style={rowStyle}>
                 <div style={inputWraperStyle}>
-                    Outfit
-                    <input type="text" style={inputStyle} value={Outfit} onChange={canModify ? (e) => setOutfit(e.target.value) : undefined}></input>
+                    Vin
+                    <input type="text" style={inputStyle} value={Vin} onChange={canModify ? (e) => setVin(e.target.value) : undefined}></input>
                 </div>
                 <div style={inputWraperStyle}>
                     Overseerer
                     <input type="text" style={inputStyle} value={Overseerer} onChange={canModify ? (e) => setOverseerer(e.target.value) : undefined}></input>
+                    {
+                        showOverseererOptions || overseererOptionsHover ? <ul style={listStyle} onMouseEnter={() => { setOvereererOptionsHover(true) }} onMouseLeave={() => { setOvereererOptionsHover(false) }}>
+                            {OverseererOptions.map(o => (
+                                <li key={o.employeeId} style={!overseererListHandler[o.employeeId] ? { cursor: "pointer" } : { cursor: "pointer", backgroundColor: "#cacaca" }} onMouseEnter={() => setOverseererHover(o.employeeId)} onClick={() => setOnCLickPurpose()}>{`${o.firstName} ${o.surname}`}</li>
+                            ))}
+                            {/*<li style={{ cursor: "pointer" }} >1</li>
+                            <li>2</li>
+                            <li>3</li>*/}
+                        </ul>
+                            : undefined
+                    }
                 </div>
                 <div style={inputWraperStyle}>
                     Serviced
