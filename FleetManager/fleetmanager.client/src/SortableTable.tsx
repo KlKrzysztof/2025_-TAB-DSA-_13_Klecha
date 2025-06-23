24d063a6c7aaba800e6db201dce1fe3f8cbed6a6
KlKrzysztof <115008405+KlKrzysztof@users.noreply.github.com>
2025-06-23 19:03:57 +0200

    Vehicles done

--- FleetManager/fleetmanager.client/src/SortableTable.tsx
+++ FleetManager/fleetmanager.client/src/SortableTable.tsx
@@ -67,7 +67,7 @@ export const SortableTable: React.FC<TableProps> = ({ fetchURL, idColumn, onRowS
                                         cursor: 'pointer',
                                         borderBottom: '1px solid #ddd',
                                         padding: '8px',
-                                        textAlign: 'left',
+                                        textAlign: 'center',
                                         background: '#f2f2f2',
                                     }}
                                 >
